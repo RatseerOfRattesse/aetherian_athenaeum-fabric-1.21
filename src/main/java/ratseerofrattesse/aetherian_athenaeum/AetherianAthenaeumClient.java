406a160b8c21bcc18b01c53f76d05794a81e12b3
TheHolyRat <ethant8251@gmail.com>
2024-11-29 18:26:19 -0800

    Initial commit! (There was a mid-setup name change so some stuff is labelled DOA8BW instead of Aetherian Athenaeum)

---
+++ src/main/java/ratseerofrattesse/aetherian_athenaeum/AetherianAthenaeumClient.java
@@ -0,0 +1,10 @@
+package ratseerofrattesse.aetherian_athenaeum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AetherianAthenaeumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ratseerofrattesse/aetherian_athenaeum/AetherianAthenaeumDataGenerator.java
@@ -0,0 +1,11 @@
+package ratseerofrattesse.aetherian_athenaeum;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AetherianAthenaeumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
